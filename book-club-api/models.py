26e69cafa0dc01506de1da016865d8775c03602b
ldscavo <logan@scavo.dev>
2024-11-17 18:50:22 -0500

    use pydantic types

--- book-club-api/models.py
+++ book-club-api/models.py
@@ -1,10 +1,11 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, AutoString
+from pydantic import EmailStr, SecretStr
 from datetime import datetime
 
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    email: str
-    password: str
+    email: EmailStr = Field(unique=True, sa_type=AutoString)
+    password: SecretStr = Field(sa_type=AutoString)
     created_at: datetime = Field(default=datetime.now())
     update_at: datetime = Field(default=datetime.now())
