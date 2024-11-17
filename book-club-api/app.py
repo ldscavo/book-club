7466d828b7072f2afd2a5dcd6d05633871d59e1f
ldscavo <logan@scavo.dev>
2024-11-17 18:50:01 -0500

    remove example routes

--- book-club-api/app.py
+++ book-club-api/app.py
@@ -1,33 +1,9 @@
-from enum import Enum
 from fastapi import FastAPI
-from dotenv import load_dotenv
 from database import create_tables
 from routers import users
-
-load_dotenv()
 
 create_tables()
 
 app = FastAPI()
 
-
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World!"}
-
 app.include_router(users.router)
-
-
-class ModelName(str, Enum):
-    foo = "foo"
-    bar = "bar"
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.foo:
-        return {"name": model_name, "value": "fighters"}
-    if model_name is ModelName.bar:
-        return {"name": model_name, "value": "none"}
-
-    return {}
