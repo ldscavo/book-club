fe972ba23214c2c7e3a4ac73b3819c06ae2a59b2
ldscavo <logan@scavo.dev>
2024-11-17 16:31:11 -0500

    add database connection and user model

--- book-club-api/app.py
+++ book-club-api/app.py
@@ -1,20 +1,36 @@
 from typing import Union
 from enum import Enum
 from fastapi import FastAPI
+from sqlmodel import SQLModel, create_engine
+from dotenv import load_dotenv
+import os
+import models
+
+
+load_dotenv()
+
 
 app = FastAPI()
+
+
+db = create_engine(os.environ.get('DATABASE_URL'))
+SQLModel.metadata.create_all(db)
+
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello World!"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 class ModelName(str, Enum):
     foo = "foo"
     bar = "bar"
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
---
+++ book-club-api/models.py
@@ -0,0 +1,10 @@
+from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: str
+    password: str
+    created_at: datetime = Field(default=datetime.now())
+    update_at: datetime
