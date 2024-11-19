669f17143bf97bfaea6c5f07f5723ddfcf47c9ec
ldscavo <logan@scavo.dev>
2024-11-19 18:38:47 -0500

    hash pw and save user

--- book-club-api/models.py
+++ book-club-api/models.py
@@ -1,11 +1,13 @@
 from sqlmodel import Field, SQLModel, AutoString
-from pydantic import EmailStr, SecretStr
+from pydantic import EmailStr
 from datetime import datetime
 
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+
     email: EmailStr = Field(unique=True, sa_type=AutoString)
-    password: SecretStr = Field(sa_type=AutoString)
+    password: bytes
+
     created_at: datetime = Field(default=datetime.now())
     update_at: datetime = Field(default=datetime.now())
--- book-club-api/routers/users.py
+++ book-club-api/routers/users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Response, status
-from pydantic import BaseModel, EmailStr, SecretStr
+from pydantic import BaseModel, EmailStr
 from sqlmodel import Session, select
+import bcrypt
 from database import db_engine
 from models import User
 
@@ -11,26 +12,34 @@ router = APIRouter(
 
 class RegistrationModel(BaseModel):
     email: EmailStr
-    password: SecretStr
-    password_confirm: SecretStr
+    password: str
+    password_confirm: str
+
+
+class Error(BaseModel):
+    msg: str
 
 
 @router.post("/register")
-async def register(reg: RegistrationModel, resp: Response) -> User | None:
+async def register(reg: RegistrationModel, resp: Response) -> User | Error:
     with Session(db_engine) as session:
         if reg.password != reg.password_confirm:
             resp.status_code = status.HTTP_400_BAD_REQUEST
-            return None
+            return Error(msg="Passwords don't match!")
 
         existing_user = session.exec(
             select(User).where(User.email == reg.email)
-        ).one_or_none
+        ).one_or_none()
 
         if existing_user is not None:
+            print(existing_user)
             resp.status_code = status.HTTP_400_BAD_REQUEST
-            return None
+            return Error(msg="User with that email already exists!")
 
-        user = User(email=reg.email)
+        password_hash = bcrypt.hashpw(
+            bytes(bytearray(reg.password, 'utf-8')), bcrypt.gensalt())
+
+        user = User(email=reg.email, password=password_hash)
         session.add(user)
         session.commit()
 
