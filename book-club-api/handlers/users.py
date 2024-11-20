7f2818e124f5056b4f83cef30ecc2f3b7dd9a716
ldscavo <logan@scavo.dev>
2024-11-19 19:26:00 -0500

    wip: initial login

--- book-club-api/handlers/users.py
+++ book-club-api/handlers/users.py
@@ -12,6 +12,11 @@ class RegistrationModel(BaseModel):
     password_confirm: str
 
 
+class LoginModel(BaseModel):
+    email: EmailStr
+    password: str
+
+
 def register_user(reg: RegistrationModel) -> User:
     with Session(db_engine) as session:
         if reg.password != reg.password_confirm:
@@ -40,4 +45,30 @@ def register_user(reg: RegistrationModel) -> User:
         session.commit()
 
         session.refresh(user)
+        return user
+
+
+def login_user(login: LoginModel) -> User:
+    with Session(db_engine) as session:
+        user = session.exec(
+            select(User).where(User.email == login.email)
+        ).one_or_none()
+
+        if user is None:
+            raise HTTPException(
+                status_code=422,
+                detail="Invalid email"
+            )
+
+        is_valid_pw = bcrypt.checkpw(
+            bytes(bytearray(login.password, 'utf-8')),
+            user.password
+        )
+
+        if not is_valid_pw:
+            raise HTTPException(
+                status_code=422,
+                detail="Invalid password"
+            )
+
         return user
--- book-club-api/routers/users.py
+++ book-club-api/routers/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Response
 from models import User
-from handlers.users import register_user, RegistrationModel
+from handlers.users import register_user, RegistrationModel, login_user, LoginModel
 
 
 router = APIRouter(prefix="/users")
@@ -9,3 +9,8 @@ router = APIRouter(prefix="/users")
 @router.post("/register")
 async def register(reg: RegistrationModel, resp: Response) -> User:
     return register_user(reg)
+
+
+@router.post("/login")
+def login(login: LoginModel, resp: Response) -> User:
+    return login_user(login)
