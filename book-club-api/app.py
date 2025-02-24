833bc3c0e826722fb19e729e8881d77d9053d474
ldscavo <logan@scavo.dev>
2025-02-23 23:43:00 -0500

    use lifespan model for db init

--- book-club-api/app.py
+++ book-club-api/app.py
@@ -1,16 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from database import create_tables
 import auth.routes as auth_routes
 import users.routes as user_routes
 import clubs.routes as club_routes
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_tables()
+    yield
 
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_routes.router)
 app.include_router(user_routes.router)
